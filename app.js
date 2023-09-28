26c701f3f2d10762c49337fb46459a870a07facf
johnray000 <johnray000@gmail.com>
2023-09-28 20:20:55 -0300

    entendiendo los headers y como evitar el cacheo

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require("express");
+import express from "express";
 var app = express();
 const PORT = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
-    res.send("probando  ")
+    res.set({'Content-Type': 'text/plain'})
+    res.send(`Hola, mundo! ${Date.now()}`)
 })
 
 app.get("/otra",(req,res)=>{
